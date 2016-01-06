6076fb687480b0a1b81ca1a07d41a635290aaf2a
Marcin Chwedczuk <chwedczukm@yahoo.com>
2016-01-06 12:52:09 +0100

    pascal triangle 2

--- src/org/mc/tests/PascalTriangleTests.java
+++ src/org/mc/tests/PascalTriangleTests.java
@@ -24,4 +24,22 @@ public class PascalTriangleTests {
             Assert.assertEquals(actualTriangle, expectedTriangle);
         }
     }
+
+    @Test
+    public void pascal_triangle_generateRow_works() {
+        int[][] pascalTriangle = new int[][] {
+                { 1 },
+                { 1, 1 },
+                { 1, 2, 1 },
+                { 1, 3, 3, 1 },
+                { 1, 4, 6, 4, 1 }
+        };
+
+        for (int row = 0; row < pascalTriangle.length; row++) {
+            int[] expectedRow = pascalTriangle[row];
+            int[] actualRow = new PascalTriangle().generateRow(row+1);
+
+            Assert.assertEquals(actualRow, expectedRow, "failed for row: " + row);
+        }
+    }
 }
--- src/org/mc/utils/PascalTriangle.java
+++ src/org/mc/utils/PascalTriangle.java
@@ -23,4 +23,21 @@ public class PascalTriangle {
 
         return rows;
     }
+
+    public int[] generateRow(int row) {
+        int[] curr = new int[row];
+        int[] last = new int[row];
+
+        for (int i = 0; i < row; i++) {
+
+            curr[0] = curr[i] = 1;
+            for (int j = 1; j < i; j++) {
+                curr[j] = last[j-1] + last[j];
+            }
+
+            int[] swap = curr; curr = last; last = swap;
+        }
+
+        return last;
+    }
 }
