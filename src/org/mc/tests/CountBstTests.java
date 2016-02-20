18b2880729a0a6657416381ea55e44fa265ea797
Marcin Chwedczuk <chwedczukm@yahoo.com>
2016-02-20 15:33:37 +0100

    generate all bst

--- src/org/mc/tests/CountBstTests.java
+++ src/org/mc/tests/CountBstTests.java
@@ -1,8 +1,11 @@
 package org.mc.tests;
 
+import org.mc.dataStructures.TreeNode;
 import org.mc.utils.CountBst;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 public class CountBstTests {
     @Test
@@ -11,5 +14,45 @@ public class CountBstTests {
         Assert.assertEquals(CountBst.withElements(1), 1);
         Assert.assertEquals(CountBst.withElements(2), 2);
         Assert.assertEquals(CountBst.withElements(3), 5);
+    }
+
+    @Test
+    public void generate_bst_works() {
+        List<TreeNode> result = CountBst.generateAllBst(0);
+        Assert.assertEquals(result.size(), 1);
+        Assert.assertEquals(result.get(0), null);
+
+        result = CountBst.generateAllBst(1);
+        Assert.assertEquals(result.size(), 1);
+        Assert.assertEquals(
+                TreeNode.treeToString(result.get(0)),
+                "(1 nil nil)");
+
+        result = CountBst.generateAllBst(2);
+        Assert.assertEquals(result.size(), 2);
+        Assert.assertEquals(
+                TreeNode.treeToString(result.get(0)),
+                "(1 nil (2 nil nil))");
+        Assert.assertEquals(
+                TreeNode.treeToString(result.get(1)),
+                "(2 (1 nil nil) nil)");
+
+        result = CountBst.generateAllBst(3);
+        Assert.assertEquals(result.size(), 5);
+        Assert.assertEquals(
+                TreeNode.treeToString(result.get(0)),
+                "(1 nil (2 nil (3 nil nil)))");
+        Assert.assertEquals(
+                TreeNode.treeToString(result.get(1)),
+                "(1 nil (3 (2 nil nil) nil))");
+        Assert.assertEquals(
+                TreeNode.treeToString(result.get(2)),
+                "(2 (1 nil nil) (3 nil nil))");
+        Assert.assertEquals(
+                TreeNode.treeToString(result.get(3)),
+                "(3 (1 nil (2 nil nil)) nil)");
+        Assert.assertEquals(
+                TreeNode.treeToString(result.get(4)),
+                "(3 (2 (1 nil nil) nil) nil)");
     }
 }
--- src/org/mc/utils/CountBst.java
+++ src/org/mc/utils/CountBst.java
@@ -1,5 +1,11 @@
 package org.mc.utils;
 
+import org.mc.dataStructures.TreeNode;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class CountBst {
     public static int withElements(int n) {
         int[] counts = new int[Math.max(2, n+1)];
@@ -15,5 +21,39 @@ public class CountBst {
         }
 
         return counts[n];
+    }
+
+    public static List<TreeNode> generateAllBst(int n) {
+        return generateAllBstImpl(1, n+1);
+    }
+
+    private static List<TreeNode> generateAllBstImpl(int start, int end) {
+        if (start >= end)
+            return single(null);
+
+        if (start+1 == end)
+            return single(new TreeNode(start));
+
+        List<TreeNode> trees = new ArrayList<>();
+
+        for (int root = start; root < end; root++) {
+            List<TreeNode> leftTrees = generateAllBstImpl(start, root);
+            List<TreeNode> rightTrees = generateAllBstImpl(root+1, end);
+
+            for (TreeNode left : leftTrees) {
+                for (TreeNode right : rightTrees) {
+                    TreeNode node = new TreeNode(root, left, right);
+                    trees.add(node);
+                }
+            }
+        }
+
+        return trees;
+    }
+
+    private static List<TreeNode> single(TreeNode value) {
+        List<TreeNode> result = new ArrayList<>();
+        result.add(value);
+        return result;
     }
 }
