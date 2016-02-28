e3b1a4f275f708264d89eccaba8d28508e4cbf91
Marcin Chwedczuk <chwedczukm@yahoo.com>
2016-02-28 11:24:24 +0100

    buy and sell 1 problem

---
+++ src/org/mc/utils/BuyAndSell1.java
@@ -0,0 +1,28 @@
+package org.mc.utils;
+
+import org.testng.annotations.Test;
+
+public class BuyAndSell1 {
+    /*
+     * Tested using: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+     * 
+     */
+    public int maxProfit(int[] prices) {
+        if (prices.length == 0)
+            return 0;
+
+        int min = prices[0];
+        int maxProfit = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] < min) {
+                min = prices[i];
+            }
+            else {
+                maxProfit = Math.max(maxProfit, prices[i]-min);
+            }
+        }
+
+        return maxProfit;
+    }
+}
